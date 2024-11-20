635dc67406fd82d0e91f2a11475dc19f46be1b91
krgarav <krgarav@gmail.com>
2024-11-20 13:51:35 +0530

    Configured ip for ubuntu

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("torrentpower", "root", "root", {
+export const sequelize = new Sequelize("torrentpower", "root", "root123@", {
     dialect: "mysql",
     // host: "database",
     // port: 3307
